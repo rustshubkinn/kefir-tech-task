52060e6ed39fb5f54408da386822a4ba6b78cbd1
rustshubkinn <rustshubkinn@mail.ru>
2024-05-20 20:15:10 +0500

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# Тестовое задание для KefirGames
-
-## Дизайн:
-
-### Figma [Comments.fig](Comments.fig)
-
-![preview.png](preview.png)
--- src/helpers/addSpacingForNumbers.ts
+++
@@ -1,6 +0,0 @@
-export const addSpacingForNumbers = (value: number) => {
-  if (typeof value !== 'number' || isNaN(value)) {
-    return null;
-  }
-  return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
-};
